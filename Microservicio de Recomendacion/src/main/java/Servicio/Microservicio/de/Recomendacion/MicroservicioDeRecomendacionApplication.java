890b9c05a42be33b201c891d5a477eb59aa14583
benjazJJ <be.palmap@duocuc.cl>
2025-04-26 16:17:38 -0400

    Agregamos carpeta de microservicio de recomendaciones

---
+++ Microservicio de Recomendacion/src/main/java/Servicio/Microservicio/de/Recomendacion/MicroservicioDeRecomendacionApplication.java
@@ -0,0 +1,13 @@
+package Servicio.Microservicio.de.Recomendacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioDeRecomendacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioDeRecomendacionApplication.class, args);
+	}
+
+}
---
+++ Microservicio de Recomendacion/src/test/java/Servicio/Microservicio/de/Recomendacion/MicroservicioDeRecomendacionApplicationTests.java
@@ -0,0 +1,13 @@
+package Servicio.Microservicio.de.Recomendacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioDeRecomendacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
