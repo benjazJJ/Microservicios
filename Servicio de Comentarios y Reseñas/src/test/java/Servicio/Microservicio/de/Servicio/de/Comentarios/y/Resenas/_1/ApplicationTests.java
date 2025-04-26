0373a217039170378d3b1f9a76db7554bedc3ee8
benjazJJ <be.palmap@duocuc.cl>
2025-04-25 22:43:54 -0400

    Add files via upload

---
+++ Servicio de Comentarios y Reseñas/src/test/java/Servicio/Microservicio/de/Servicio/de/Comentarios/y/Resenas/_1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Servicio.Microservicio.de.Servicio.de.Comentarios.y.Resenas._1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
