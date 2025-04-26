ba43f96b34419c691944be5885b0d9574e64e765
benjazJJ <be.palmap@duocuc.cl>
2025-04-26 16:07:49 -0400

    Ultimos cambios
    Eliminé repositorios duplicados

---
+++ Microservicio de Control de roles y Permisos/src/test/java/Servicio/Microservicio/de/Control/de/roles/y/Permisos/MicroservicioDeControlDeRolesYPermisosApplicationTests.java
@@ -0,0 +1,13 @@
+package Servicio.Microservicio.de.Control.de.roles.y.Permisos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioDeControlDeRolesYPermisosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
