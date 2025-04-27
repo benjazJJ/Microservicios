c63122dcac8b0ab4582952783d74a3912d99b289
benjazJJ <be.palmap@duocuc.cl>
2025-04-27 18:04:43 -0400

    Microservicio Catalogo Libros
    Este microservicio está funcionando correctamente, hice prueas en postman, GET, POST, PUT Y DELETE. Funciona todo correctamente
    
    Co-Authored-By: Soupyyyhh <167207921+Soupyyyhh@users.noreply.github.com>
    Co-Authored-By: Gustavo Espinoza <170636201+DEALTONICOX@users.noreply.github.com>

--- Microservicio de Catalogo de Libros/src/main/java/Servicio/Microservicio/de/Catalogo/de/Libros2/Service/LibroService.java
+++ Microservicio de Catalogo de Libros/src/main/java/Servicio/Microservicio/de/Catalogo/de/Libros2/Service/LibroService.java
@@ -1,5 +1,41 @@
 package Servicio.Microservicio.de.Catalogo.de.Libros2.Service;
 
+import Servicio.Microservicio.de.Catalogo.de.Libros2.Model.Libro;
+import Servicio.Microservicio.de.Catalogo.de.Libros2.Repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class LibroService {
 
+    @Autowired
+    private LibroRepository libroRepository;
+
+    // Obtener todos los libros
+    public List<Libro> getLibros() {
+        return libroRepository.obtenerLibros();
+    }
+
+    // Guardar un nuevo libro
+    public Libro saveLibro(Libro libro) {
+        return libroRepository.guardar(libro);
+    }
+
+    // Buscar un libro por ID
+    public Libro getLibroById(int idLibro) {
+        return libroRepository.buscarPorId(idLibro);
+    }
+
+    // Actualizar un libro existente
+    public Libro updateLibro(Libro libro) {
+        return libroRepository.actualizar(libro);
+    }
+
+    // Eliminar un libro por su ID
+    public String deleteLibro(int idLibro) {
+        libroRepository.eliminar(idLibro);
+        return "Libro eliminado correctamente";
+    }
 }
