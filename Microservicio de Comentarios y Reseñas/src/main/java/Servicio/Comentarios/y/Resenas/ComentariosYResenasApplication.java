e196f8d39147ea76acaad6b3a9f447e4c67928a5
Gustavo Espinoza <gust.espinoza@duocuc.cl>
2025-05-02 18:13:36 -0400

    Añadidos
    Añadi 2 microservicio
    -Microservicio Carrito
    -Microservicio Comentarios y reseñas

---
+++ Microservicio de Comentarios y Reseñas/src/main/java/Servicio/Comentarios/y/Resenas/ComentariosYResenasApplication.java
@@ -0,0 +1,13 @@
+package Servicio.Comentarios.y.Resenas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComentariosYResenasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComentariosYResenasApplication.class, args);
+	}
+
+}
---
+++ Microservicio de Comentarios y Reseñas/src/test/java/Servicio/Comentarios/y/Resenas/ComentariosYResenasApplicationTests.java
@@ -0,0 +1,13 @@
+package Servicio.Comentarios.y.Resenas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComentariosYResenasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
