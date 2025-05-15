9be9d87748884e023a58dd3918985c9a852b07e5
benjazJJ <be.palmap@duocuc.cl>
2025-05-15 19:19:28 -0400

    Add files via upload

---
+++ Microservicio-de-Solicitudes-de-Ayuda/src/main/java/Servicio/Microservicio/de/Solicitudes/de/Ayuda/MicroservicioDeSolicitudesDeAyudaApplication.java
@@ -0,0 +1,13 @@
+package Servicio.Microservicio.de.Solicitudes.de.Ayuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioDeSolicitudesDeAyudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioDeSolicitudesDeAyudaApplication.class, args);
+	}
+
+}
