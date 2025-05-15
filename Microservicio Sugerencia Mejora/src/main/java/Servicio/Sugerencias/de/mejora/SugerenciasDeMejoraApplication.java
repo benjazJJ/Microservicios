b3a6aaabf14b01ec219ae2c083bc594d441c5eac
benjazJJ <be.palmap@duocuc.cl>
2025-05-15 19:08:45 -0400

    Add files via upload

---
+++ Microservicio Sugerencia Mejora/src/main/java/Servicio/Sugerencias/de/mejora/SugerenciasDeMejoraApplication.java
@@ -0,0 +1,13 @@
+package Servicio.Sugerencias.de.mejora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugerenciasDeMejoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SugerenciasDeMejoraApplication.class, args);
+	}
+
+}
