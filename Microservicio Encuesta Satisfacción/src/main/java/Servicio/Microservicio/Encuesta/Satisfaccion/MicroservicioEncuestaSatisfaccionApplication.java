7a4686afadce08b735b31795ccf2e61c93bd7e57
benjazJJ <be.palmap@duocuc.cl>
2025-05-15 19:11:52 -0400

    Add files via upload

---
+++ Microservicio Encuesta Satisfacción/src/main/java/Servicio/Microservicio/Encuesta/Satisfaccion/MicroservicioEncuestaSatisfaccionApplication.java
@@ -0,0 +1,13 @@
+package Servicio.Microservicio.Encuesta.Satisfaccion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioEncuestaSatisfaccionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioEncuestaSatisfaccionApplication.class, args);
+	}
+
+}
