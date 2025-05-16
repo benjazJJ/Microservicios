e56d81008eab60e5f9588dc502e8961d3665adb0
benjazJJ <be.palmap@duocuc.cl>
2025-05-15 21:06:23 -0400

    Eliminamos este microservicio para añadir el de configuración de cuenta

--- Microservicio-de-Solicitudes-de-Ayuda/src/main/java/com/ayuda/repository/SolicitudAyudaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ayuda.repository;
-
-import com.ayuda.model.SolicitudAyuda;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SolicitudAyudaRepository extends JpaRepository<SolicitudAyuda, Integer> {
-}
