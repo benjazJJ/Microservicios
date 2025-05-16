c1c71084049c5545088fec23cafb7c5b667aa18e
benjazJJ <be.palmap@duocuc.cl>
2025-05-15 20:34:16 -0400

    Cambios en microservicio sugerencia de mejora

--- Microservicio Sugerencias de mejora/src/main/java/com/sugerencias/model/SugerenciaMejora.java
+++ Microservicio Sugerencias de mejora/src/main/java/com/sugerencias/model/SugerenciaMejora.java
@@ -1,11 +1,15 @@
 package com.sugerencias.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "sugerencias_mejora")
 public class SugerenciaMejora {
@@ -24,3 +28,4 @@ public class SugerenciaMejora {
     @Column(name = "fecha_envio")
     private LocalDateTime fechaEnvio;
 }
+
