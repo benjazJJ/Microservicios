e820302a16c94bf7618e6298370c1c0715b89d2e
benjazJJ <be.palmap@duocuc.cl>
2025-05-15 21:07:04 -0400

    Microservicio de configuración de cuenta
    este debe realizarlo @gustavo y @benjaminleal

---
+++ Microservicio de Configuración de Cuenta/src/main/java/Servicio/Microservicio/de/Configuracion/de/Cuenta/MicroservicioDeConfiguracionDeCuentaApplication.java
@@ -0,0 +1,13 @@
+package Servicio.Microservicio.de.Configuracion.de.Cuenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioDeConfiguracionDeCuentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioDeConfiguracionDeCuentaApplication.class, args);
+	}
+
+}
---
+++ Microservicio de Configuración de Cuenta/src/test/java/Servicio/Microservicio/de/Configuracion/de/Cuenta/MicroservicioDeConfiguracionDeCuentaApplicationTests.java
@@ -0,0 +1,13 @@
+package Servicio.Microservicio.de.Configuracion.de.Cuenta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioDeConfiguracionDeCuentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
