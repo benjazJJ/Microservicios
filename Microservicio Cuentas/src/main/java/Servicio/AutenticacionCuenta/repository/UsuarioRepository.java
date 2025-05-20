0e42fb016d84af03ad904b3d6a5326f8a0a958cb
Soupyyyhh <be.leall@duocuc.cl>
2025-05-20 17:36:37 -0400

    Microservicio LibroStock
    Hice denuevo este microservicio de acuerdo a los requerimientos
    funciona en las 4 opciones Get, Post, Put, delete.

--- Microservicio Cuentas/src/main/java/Servicio/AutenticacionCuenta/repository/UsuarioRepository.java
+++ Microservicio Cuentas/src/main/java/Servicio/AutenticacionCuenta/repository/UsuarioRepository.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import Servicio.AutenticacionCuenta.model.Usuario;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     Optional<Usuario> findByCorreo(String correo);
 
     Optional<Usuario> findByRut(String rut);
