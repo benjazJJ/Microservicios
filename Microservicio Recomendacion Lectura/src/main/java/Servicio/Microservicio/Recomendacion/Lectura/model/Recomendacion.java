e33ec511c9169d3470476c1e42e0a70d6e174e60
benjazJJ <be.palmap@duocuc.cl>
2025-05-25 00:11:02 -0400

    Añadi columnas

--- Microservicio Recomendacion Lectura/src/main/java/Servicio/Microservicio/Recomendacion/Lectura/model/Recomendacion.java
+++ Microservicio Recomendacion Lectura/src/main/java/Servicio/Microservicio/Recomendacion/Lectura/model/Recomendacion.java
@@ -14,10 +14,18 @@ public class Recomendacion {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
 
+    @Column(name = "titulo_libro")
     private String tituloLibro;
+
+    @Column(name = "autor")
     private String autor;
+
+    @Column(name = "categoria")
     private String categoria;
+
+    @Column(name = "motivo")
     private String motivo;
 }
