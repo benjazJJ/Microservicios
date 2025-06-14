8a518566b2817be856cc01066f5630af2c5dad27
Gustavo Espinoza <gust.espinoza@duocuc.cl>
2025-06-14 19:01:28 -0400

    Delete Microservicio Encuesta Satisfacción directory

--- Microservicio Encuesta Satisfacción/src/main/java/com/encuesta/repository/EncuestaSatisfaccionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.encuesta.repository;
-
-import com.encuesta.model.EncuestaSatisfaccion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EncuestaSatisfaccionRepository extends JpaRepository<EncuestaSatisfaccion, Integer> {
-}
