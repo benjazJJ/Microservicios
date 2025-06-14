22944e5d01bdc7df07e06380b8f1bbd321e0e103
Gustavo Espinoza <gust.espinoza@duocuc.cl>
2025-06-14 19:01:03 -0400

    Delete Microservicio Sugerencias de mejora directory
    Borro este microservicio para juntar sugerencias con Encuesta

--- Microservicio Sugerencias de mejora/src/main/java/com/sugerencias/repository/SugerenciaMejoraRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sugerencias.repository;
-
-import com.sugerencias.model.SugerenciaMejora;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SugerenciaMejoraRepository extends JpaRepository<SugerenciaMejora, Integer> {
-}
