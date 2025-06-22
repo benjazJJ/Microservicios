de3d1a681193c7ddff367171e6789959ebf16e92
Gustavo Espinoza <gust.espinoza@duocuc.cl>
2025-06-22 13:34:17 -0400

    Add files via upload

---
+++ Solicitudes-de-Ayuda/src/main/java/Microservicio/Solicitudes/de/Ayuda/SolicitudesDeAyudaApplication.java
@@ -0,0 +1,13 @@
+package Microservicio.Solicitudes.de.Ayuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolicitudesDeAyudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolicitudesDeAyudaApplication.class, args);
+	}
+
+}
---
+++ Solicitudes-de-Ayuda/src/test/java/Microservicio/Solicitudes/de/Ayuda/SolicitudesDeAyudaApplicationTests.java
@@ -0,0 +1,13 @@
+package Microservicio.Solicitudes.de.Ayuda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolicitudesDeAyudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
