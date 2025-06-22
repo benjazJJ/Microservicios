6ab6ee231efd0f081d25531e10f74b8e86d10eb4
Gustavo Espinoza <gust.espinoza@duocuc.cl>
2025-06-22 14:07:57 -0400

    Delete SolicitudesAyuda/Solicitudes-de-Ayuda directory

--- SolicitudesAyuda/Solicitudes-de-Ayuda/src/main/java/Microservicio/Solicitudes/de/Ayuda/WebClient/ValidacionResponse.java
+++
@@ -1,9 +0,0 @@
-package Microservicio.Solicitudes.de.Ayuda.WebClient;
-
-import lombok.Data;
-
-@Data
-public class ValidacionResponse {
-    private boolean autenticado;
-    private String rol;
-}
